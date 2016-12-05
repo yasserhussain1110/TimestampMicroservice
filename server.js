85bc9ef3b3d24058fa74844abdb7c355a187aeb9
Yasser Hussain <yasserhussain1110@gmail.com>
2016-12-05 13:40:39 +0530

    Finished app

--- server.js
+++ server.js
@@ -1,8 +1,31 @@
+var path = require('path');
 var express = require('express');
+var strftime = require('strftime');
+
+
 var app = express();
 
-app.get('/test', function(req, res) {
-  res.send("hello!");
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/:date', function(req, res) {
+  var inputDate = req.params.date;
+  var naturalDate, unixDate;
+
+  if (!isNaN(inputDate)) {
+    unixDate = Number(inputDate)*1000;
+    naturalDate = strftime('%B %d, %Y', new Date(unixDate));
+  } else {
+    naturalDate = inputDate;
+    unixDate = Date.parse(naturalDate);
+    if (isNaN(unixDate)) {
+      naturalDate = null;
+    }
+  }
+
+  res.json({
+    unix: unixDate,
+    natural: naturalDate
+  });
 });
 
 app.listen(8080);
